e76317b076703767e581699f74b9ed1d6e3e0249
Irfan Maulana <irfanmaulana126@gmail.com>
2021-06-26 12:28:15 +0700

    add image guide

--- src/models/guideModel.js
+++ src/models/guideModel.js
@@ -4,7 +4,7 @@ const dbConn = require('../../config/db.config');
 
 const getGuide = (req ,res) =>{
     req.risk = '%' + req.risk + '%';
-    dbConn.query("Select a.* from guide_bencana as a inner join tbl_bencana as b on b.id = a.type where b.name like N?",[req.risk],(err,results)=>{
+    dbConn.query("Select a.*,b.image from guide_bencana as a inner join tbl_bencana as b on b.id = a.type where b.name like N?",[req.risk],(err,results)=>{
         if (err) {
             console.log(err);
             res(err, null);
