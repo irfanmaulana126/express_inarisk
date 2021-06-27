a4037ae2c106fdb63946502b9f06f460aaa60f9a
Irfan Maulana <irfanmaulana126@gmail.com>
2021-06-27 16:54:53 +0700

    add ? in query

--- src/models/guideModel.js
+++ src/models/guideModel.js
@@ -3,7 +3,7 @@
 const dbConn = require('../../config/db.config');
 
 const getGuide = (req ,res) =>{
-    dbConn.query("Select a.*,b.image from guide_bencana as a inner join tbl_bencana as b on b.id = a.type where a.type",[req.id],(err,results)=>{
+    dbConn.query("Select a.*,b.image from guide_bencana as a inner join tbl_bencana as b on b.id = a.type where a.type=?",[req.id],(err,results)=>{
         if (err) {
             console.log(err);
             res(err, null);
